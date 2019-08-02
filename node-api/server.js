3b4a7c36605be1776c463a75e81ecacf582cab7b
tukno <oliveiradavid007@gmail.com>
2019-08-02 18:45:25 -0400

    Created first route and added some dependencies

--- node-api/server.js
+++ node-api/server.js
@@ -3,5 +3,12 @@ const express = require('express');
 
 const app = express();
 
+//req: It represents the request to the server from a host
+// (include parameteres, headers, ip, user authorization, etc)
+//res: It represents the response to a host
+app.get('/', (req, res) => {
+    res.send('Hello Rocketseat!');
+});
+
 //port to listen
 app.listen(3001);
