f415afabe02d35b7e1d7c03e1c66e7bf75de672e
tukno <oliveiradavid007@gmail.com>
2019-08-03 02:03:53 -0400

    Setting mongoose

--- node-api/server.js
+++ node-api/server.js
@@ -1,8 +1,16 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
 
+//Starting the app
 const app = express();
 
+//Starting database
+mongoose.connect('mongodb://localhost:27017/node-api', { useNewUrlParser: true });
+
+
+
+// Creating our first route
 //req: It represents the request to the server from a host
 // (include parameteres, headers, ip, user authorization, etc)
 //res: It represents the response to a host
